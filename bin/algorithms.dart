bf8b61a3def77ef302dfe359f9d951cb00bcf851
Mahmoud <md.thunder2014@gmail.com>
2022-05-23 16:21:34 +0300

    commit

--- bin/algorithms.dart
+++ bin/algorithms.dart
@@ -1,28 +1,70 @@
 void main(List<String> args) {
-  List<int> lst = [10, 5, -5, 84, 11, 65, 2, 98, 100, 556, 12];
-  print(bubbleSortAlg(lst));
+  List<int> lst = [0, 5, -5, 84, 11, 65, 2, 98, 100, 556, 12];
+  print(selectionSortAlg(lst));
 }
 
-List<int> bubbleSortAlg(List<int> list) {
+List<int> selectionSortAlg(List<int> list) {
+  int min = 0;
   int counter = 0;
-  bool noSwap;
-  for (int i = list.length; i > 0; i--) {
-    noSwap = true;
-    for (int j = 0; j < i - 1; j++) {
+  for (int i = 0; i < list.length; i++) {
+    int min = i;
+    print('==============outside==============');
+    print(list);
+    for (int j = i + 0; j < list.length; j++) {
       counter++;
-      if (list[j] > list[j + 1]) {
-        int num = list[j];
-        list[j] = list[j + 1];
-        list[j + 1] = num;
-        noSwap = false;
+      if (list[j] < list[min]) {
+        min = j;
+        // list[j + 1] = list[j];
+        // list[j] = num;
+
       }
+      print('==============inside==============');
+      print(list);
+      print(list[min]);
     }
-    if (noSwap) break;
+
+    if (min != i) {
+      int tmp = list[i];
+      list[i] = list[min];
+      list[min] = tmp;
+      print('Swap done');
+    }
+    print('****************************');
   }
-  print(counter);
+
   print(counter);
   return list;
 }
+
+
+
+//.............................................
+// 39 bubble sort algorithms  
+//void main(List<String> args) {
+//   List<int> lst = [10, 5, -5, 84, 11, 65, 2, 98, 100, 556, 12];
+//   print(bubbleSortAlg(lst));
+// }
+
+// List<int> bubbleSortAlg(List<int> list) {
+//   int counter = 0;
+//   bool noSwap;
+//   for (int i = list.length; i > 0; i--) {
+//     noSwap = true;
+//     for (int j = 0; j < i - 1; j++) {
+//       counter++;
+//       if (list[j] > list[j + 1]) {
+//         int num = list[j];
+//         list[j] = list[j + 1];
+//         list[j + 1] = num;
+//         noSwap = false;
+//       }
+//     }
+//     if (noSwap) break;
+//   }
+
+//   print(counter);
+//   return list;
+// }
 
 
 // 38 bubble sort algorithms  قبل التحديث
